d7458673fe9c4232ccddd8b5c9f28db7035377d6
Baactiba <dieplover@gmail.com>
2024-05-05 01:02:40 -0500

    Rearranged files + added EXE

--- README.md
+++
@@ -1,2 +0,0 @@
-# 2048-3x3-Trainer
-An application that can be used to learn 2048 3x3 strategies, play for score, and test your skill.
--- Test.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-public class Test
-{
-	static public void main (String args[]) throws IOException
-	{
-	}
-}
